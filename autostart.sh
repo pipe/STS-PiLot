fe8a95aceb36e7e6fe67867584bdf71eca1fb521
Tim Panton <tim@pi.pe>
2019-08-22 15:20:53 +0000

    Added drive/learn/auto choice

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Autodetect username
-USER=$(stat -c '%U' $0)
-# Uncomment following line to manually configure username
-USER="root"
+USER=mendel
 # Absolute path to this script.
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in.
 SCRIPTPATH=$(dirname $SCRIPT)
-su -c "python $SCRIPTPATH/app.py >/dev/null 2>&1 &" $USER 
+GSCRIPTPATH=/project2/github/pipe/google-coral-examples-camera-pipe/gstreamer
+PSCRIPTPATH=/home/mendel/webcam
+python3 $SCRIPTPATH/app.py >/dev/null 2>&1 & 
+sleep 2
+chgrp i2c /sys/class/pwm/pwmchip1/pwm0/* && chmod -R g+w /sys/class/pwm/pwmchip1/pwm0/*
+chgrp i2c /sys/class/pwm/pwmchip0/pwm0/* && chmod -R g+w /sys/class/pwm/pwmchip0/pwm0/*
+#su -c "$GSCRIPTPATH/start.sh >/dev/null 2>&1 &" $USER 
+sleep 1
+su -c "$PSCRIPTPATH/start.sh >/dev/null 2>&1 &" $USER 
+exit 0
 
 
